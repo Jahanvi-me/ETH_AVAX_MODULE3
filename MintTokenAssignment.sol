cb252d8fdf2576500bb487081b90f31088195d5b
Jahanvi-me <139259201+Jahanvi-me@users.noreply.github.com>
2023-08-30 22:30:08 +0530

    Update MintTokenAssignment.sol

--- MintTokenAssignment.sol
+++ MintTokenAssignment.sol
@@ -4,13 +4,22 @@ pragma solidity ^0.8.18;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract Jahanvi_Token is ERC20("THE TOKEN", "TH"), Ownable{
+contract Jahanvi_Token is ERC20("THE TOKEN", "TH"), Ownable {
         
-        function MintToken(address mintTo, uint amount) public onlyOwner{
-            _mint(mintTo, amount* 10**18);
-        }
+    constructor(uint256 initialSupply) {
+        _mint(msg.sender, initialSupply * 10**18);
+    }
 
-        function BurnToken(uint amount) public {
-            _burn(msg.sender, amount* 10**18);
-        }
+    function TransferToken(address to, uint256 amount) public returns (bool) {
+        _transfer(msg.sender, to, amount);
+        return true;
+    }
+
+    function MintToken(address mintTo, uint256 amount) public onlyOwner {
+        _mint(mintTo, amount * 10**18);
+    }
+
+    function BurnToken(uint256 amount) public {
+        _burn(msg.sender, amount * 10**18);
+    }
 }
